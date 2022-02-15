28496381db4b9e9147f7cc8e285c8497ce06c142
wozniak04 <mikimen321@gmail.com>
2022-02-15 22:18:53 +0100

    .

--- sklep/Register.cs
+++ sklep/Register.cs
@@ -18,8 +18,112 @@ namespace sklep
             InitializeComponent();
             userAwatar.Image = userAwatar_List.Images[0];
         }
+        private void User_Clear(object sender, EventArgs e)
+        {
+            if (tUser.Text == "Nazwa użytkownika")
+            {
+                tUser.Clear();
+            }
+        }
 
-        
+        private void User_Return(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tUser.Text))
+            {
+                tUser.Text = "Nazwa użytkownika";
+            }
+        }
+
+        private void Password_Clear(object sender, EventArgs e)
+        {
+            if (tPassword.Text == "Hasło")
+            {
+                tPassword.Clear();
+            }
+        }
+
+        private void Password_Return(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tPassword.Text))
+            {
+                tPassword.Text = "Hasło";
+            }
+        }
+
+        private void Email_Clear(object sender, EventArgs e)
+        {
+            if (tEmail.Text == "Email")
+            {
+                tEmail.Clear();
+            }
+        }
+
+        private void Email_Return(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tEmail.Text))
+            {
+                tEmail.Text = "Email";
+            }
+        }
+
+        private void Login_Clear(object sender, EventArgs e)
+        {
+            if (tLogin.Text == "Login")
+            {
+                tLogin.Clear();
+            }
+        }
+
+        private void Login_Return(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tLogin.Text))
+            {
+                tLogin.Text = "Login";
+            }
+        }
+
+
+
+
+        //information about registration
+        private void btnRegister_Apply_Click_1(object sender, EventArgs e)
+        {
+            Register_Alert regAlert = new Register_Alert();
+            regAlert.Show();
+        }
+
+        //return to main window
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            var Logging = new Logging();
+            Logging.ShowDialog();
+            this.Close();
+        }
+
+        //Changing the user image functions
+        int count = -1;
+        private void userChange_Left_Click(object sender, EventArgs e)
+        {
+            if (count > 0)
+            {
+                count--;
+            }
+
+            userAwatar.Image = userAwatar_List.Images[count];
+        }
+
+        private void userChange_Right_Click(object sender, EventArgs e)
+        {
+
+            if (count < 12)
+            {
+                count++;
+            }
+            userAwatar.Image = userAwatar_List.Images[count];
+        }
+
+
     }
 }
 
