2fc11d0720cc572e50be46bd79b93f0c72c9d198
wozniak04 <mikimen321@gmail.com>
2022-02-16 18:36:25 +0100

    Create dbo.Person.sql

---
+++ sklep/dbo.Person.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Person] (
+    [Id]    INT          NOT NULL,
+    [Nazwa] VARCHAR (21) NOT NULL,
+    [Haslo] VARCHAR (50) NOT NULL,
+    [Email] VARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+INSERT INTO Person (Nazwa,Haslo,Email) VALUES ("admin","root","Email")
