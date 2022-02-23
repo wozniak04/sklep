a2ebb7a32561f3dc4d8170af3c5af02d86194167
wozniak04 <mikimen321@gmail.com>
2022-02-23 22:43:30 +0100

    .

--- Users/dbo/tables/Oferty.sql
+++ Users/dbo/tables/Oferty.sql
@@ -1,4 +1,8 @@
 ﻿CREATE TABLE [dbo].[Oferty]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Nazwa] NVARCHAR(50) NOT NULL, 
+    [Cena] INT NOT NULL, 
+    [Zdjecie] IMAGE NOT NULL, 
+    [Opis] NTEXT NOT NULL
 )
