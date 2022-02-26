d69cfb0c67f4decca49e10108aab6c9e3c490211
Freedy-R <dedspinpolski@gmail.com>
2022-02-26 17:45:39 +0100

    .

--- sklep/Shop.Designer.cs
+++ sklep/Shop.Designer.cs
@@ -428,7 +428,6 @@ namespace sklep
             // pictureBox3
             // 
             this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
             this.pictureBox3.Location = new System.Drawing.Point(4, 4);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(441, 347);
